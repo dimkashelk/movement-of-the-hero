f8e7c7a77b600aa95c08d6d92e7eb1bdfd2890c0
shelk <shelk04@mail.ru>
2020-03-01 22:02:04 +0300

    Новый уровень

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
 import os
+from copy import deepcopy
 
 pygame.init()
-MONITOR_width, MONITOR_height = 200, 200
+MONITOR_width, MONITOR_height = 600, 600
 size = (MONITOR_width, MONITOR_height)
 screen = pygame.display.set_mode(size)
 
@@ -73,20 +74,21 @@ class Player(pygame.sprite.Sprite):
         self.rect = dop
 
 
-def generate_level(level):
+def generate_level(level, pr=True):
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
                 Tile('empty', x, y)
-            elif level[y][x] == '#':
+            if level[y][x] == '#':
                 Tile('wall', x, y)
-            elif level[y][x] == '@':
+            if pr and level[y][x] == '@':
                 Tile('empty', x, y)
                 new_player = Player(x, y)
-                player_x, player_y = x, y
+            else:
+                Tile('empty', x, y)
     # вернем игрока, а также размер поля в клетках
-    return new_player, x, y, player_x, player_y
+    return new_player, x, y
 
 
 player = None
@@ -104,8 +106,9 @@ except FileNotFoundError:
     print('File not found!')
     exit(0)
 camera = Camera()
-player, level_x, level_y, player_x, player_y = generate_level(card)
+player, level_x, level_y = generate_level(card)
 camera.update(player)
+x, y = 90, 90
 for sprite in all_sprites:
     camera.apply(sprite)
 while running:
@@ -116,32 +119,55 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
             elif event.key == pygame.K_DOWN:
-                player_y += 1
-                if player_y >= level_y:
-                    player_y -= 1
-                if card[player_x][player_y] == '#':
-                    player_y -= 1
+                player.rect.y += 10
+                y += 10
+                if y >= height - height // 4:
+                    dop = deepcopy(card[0])
+                    del card[0]
+                    card.append(dop)
+                    y -= 100
+                    all_sprites = pygame.sprite.Group()
+                    all_sprites.add(player)
+                    tiles_group = pygame.sprite.Group()
+                    generate_level(card, pr=False)
             elif event.key == pygame.K_UP:
-                player_y -= 1
-                if player_y >= level_y:
-                    player_y += 1
-                if card[player_x][player_y] == '#':
-                    player_y += 1
+                player.rect.y -= 10
+                y -= 10
+                if y <= height // 4:
+                    dop = deepcopy(card[-1])
+                    del card[-1]
+                    card.insert(0, dop)
+                    y += 100
+                    all_sprites = pygame.sprite.Group()
+                    all_sprites.add(player)
+                    tiles_group = pygame.sprite.Group()
+                    generate_level(card, pr=False)
             elif event.key == pygame.K_RIGHT:
-                player_x += 1
-                if player_x >= level_x:
-                    player_x -= 1
-                if card[player_x][player_y] == '#':
-                    player_x -= 1
+                player.rect.x += 10
+                x += 10
+                if x >= width - width // 4:
+                    for i, value in enumerate(card):
+                        card[i] = value[1:] + value[0]
+                    x -= 100
+                    all_sprites = pygame.sprite.Group()
+                    all_sprites.add(player)
+                    tiles_group = pygame.sprite.Group()
+                    generate_level(card, pr=False)
             elif event.key == pygame.K_LEFT:
-                player_x -= 1
-                if player_x >= level_x:
-                    player_x += 1
-                if card[player_x][player_y] == '#':
-                    player_x += 1
-            player.move(player_x, player_y)
+                player.rect.x -= 10
+                x -= 10
+                if x <= width // 4:
+                    for i, value in enumerate(card):
+                        card[i] = value[-1] + value[:-1]
+                    x -= 100
+                    all_sprites = pygame.sprite.Group()
+                    all_sprites.add(player)
+                    tiles_group = pygame.sprite.Group()
+                    generate_level(card, pr=False)
     camera.update(player)
     for sprite in all_sprites:
         camera.apply(sprite)
-    all_sprites.draw(screen)
+    screen.fill((0, 0, 0))
+    tiles_group.draw(screen)
+    player_group.draw(screen)
     pygame.display.flip()
